29b8c993d4e37991469076a6d4f7a66918a4cb90
Johan Broberg <johan@pontemonti.net>
2025-11-12 21:49:04 -0800

    Update __init__.py

--- libraries/microsoft-agents-a365-tooling-extensions-semantickernel/microsoft_agents_a365/tooling/extensions/semantickernel/__init__.py
+++ libraries/microsoft-agents-a365-tooling-extensions-semantickernel/microsoft_agents_a365/tooling/extensions/semantickernel/__init__.py
@@ -1,4 +1,5 @@
-# Copyright (c) Microsoft. All rights reserved.
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
 
 """
 Semantic Kernel extensions for Microsoft Agent 365 Tooling SDK
