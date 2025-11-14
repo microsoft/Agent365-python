7f408bad003eea40ce82f77b105496ea2ed2539e
Johan Broberg <johan@pontemonti.net>
2025-11-13 21:49:00 -0800

    Update copyright notice in integration tests

--- tests/observability/extensions/agentframework/integration/__init__.py
+++ tests/observability/extensions/agentframework/integration/__init__.py
@@ -1,4 +1,4 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
-"""Integration tests for Agent365 Python SDK."""
+"""Integration tests for Microsoft Agent 365 Python SDK."""
