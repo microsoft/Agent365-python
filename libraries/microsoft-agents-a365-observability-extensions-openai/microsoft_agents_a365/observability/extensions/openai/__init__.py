96bb458cedda12f73c1b444fa371445eee72b98c
Johan Broberg <johan@pontemonti.net>
2025-11-13 21:31:03 -0800

    Update documentation for Agent 365 Telemetry Solution

--- libraries/microsoft-agents-a365-observability-extensions-openai/microsoft_agents_a365/observability/extensions/openai/__init__.py
+++ libraries/microsoft-agents-a365-observability-extensions-openai/microsoft_agents_a365/observability/extensions/openai/__init__.py
@@ -1,7 +1,7 @@
 # Copyright (c) Microsoft. All rights reserved.
 
 """
-Wraps the OpenAI Agents SDK tracer to integrate with our Agent365 Telemetry Solution.
+Wraps the OpenAI Agents SDK tracer to integrate with the Microsoft Agent 365 Telemetry Solution.
 """
 
 from .trace_instrumentor import OpenAIAgentsTraceInstrumentor
