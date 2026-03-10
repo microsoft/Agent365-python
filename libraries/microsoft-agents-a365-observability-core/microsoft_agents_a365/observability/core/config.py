46f9c6ecb0854e125b0aae231982d60903bd2401
Rodrigo Brandao <robrandao@microsoft.com>
2026-03-10 11:58:00 -0700

    changing module used to import default OTLP exporter

--- libraries/microsoft-agents-a365-observability-core/microsoft_agents_a365/observability/core/config.py
+++ libraries/microsoft-agents-a365-observability-core/microsoft_agents_a365/observability/core/config.py
@@ -8,7 +8,7 @@ from collections.abc import Callable
 from typing import Any, Optional
 
 from opentelemetry import trace
-from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk.resources import SERVICE_NAME, SERVICE_NAMESPACE, Resource
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import ConsoleSpanExporter
